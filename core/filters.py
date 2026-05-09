fc765ee8f1db5ea699b1ee0460508501344a6aa5
Muhammad Arslan <m.arslanarain786@gmail.com>
2026-05-09 17:25:22 +0500

    V5.0.2

--- core/filters.py
+++ core/filters.py
@@ -33,12 +33,12 @@ class TransactionFilter(django_filters.FilterSet):
     date_after = django_filters.DateFilter(
         field_name='date', lookup_expr='gte',
         label="From Date",
-        widget=forms.DateInput(attrs={'class': 'datepicker-input form-input block w-full border-gray-200 rounded-lg  focus:ring-brand-500 text-sm py-2.5 px-4 bg-white', 'placeholder': 'From Date', 'type': 'date'})
+        widget=forms.DateInput(attrs={'class': 'datepicker-input form-input block w-full border-gray-200 rounded-lg  focus:ring-brand-500 text-sm py-2.5 px-4 bg-white', 'placeholder': 'From Date', 'type': 'date', 'data-default-today': 'true'})
     )
     date_before = django_filters.DateFilter(
         field_name='date', lookup_expr='lte',
         label="To Date",
-        widget=forms.DateInput(attrs={'class': 'datepicker-input form-input block w-full border-gray-200 rounded-lg  focus:ring-brand-500 text-sm py-2.5 px-4 bg-white', 'placeholder': 'To Date', 'type': 'date'})
+        widget=forms.DateInput(attrs={'class': 'datepicker-input form-input block w-full border-gray-200 rounded-lg  focus:ring-brand-500 text-sm py-2.5 px-4 bg-white', 'placeholder': 'To Date', 'type': 'date', 'data-default-today': 'true'})
     )
 
     def filter_duration(self, queryset, name, value):
